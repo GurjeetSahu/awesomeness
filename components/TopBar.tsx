a497b060fbc2efb08040666e3173f803d78af142
Gurjeet Sahu <gurjeetsahu91425@gmail.com>
2025-12-02 01:26:11 +0530

    fetch onLoad topbar

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -2,7 +2,31 @@
 import { ThemeChanger } from "@/components/ThemeChanger";
 import { User } from "@/components/User";
 import Ai from "@/components/Ai";
+import { useEffect } from "react";
+import { useQueryClient } from "@tanstack/react-query";
+import { Button } from "./ui/button";
+
 export default function TopBar() {
+  const queryClient = useQueryClient();
+  useEffect(() => {
+    if (typeof window === "undefined") return;
+
+    const cursor = localStorage.getItem("cursor");
+
+    if (!cursor) {
+      console.log("No cursor found");
+
+      const unsubscribe = queryClient.getQueryCache().subscribe(() => {
+        const q = queryClient.getQueryData(["all-stars"]);
+        if (q) {
+          console.log("All-stars query detected → invalidating...");
+          queryClient.invalidateQueries({ queryKey: ["all-stars"] });
+          unsubscribe();
+        }
+      });
+    }
+  }, []);
+
   return (
     <div className="flex items-center justify-between bg-gray-800 p-4">
       <div className="text-white text-lg font-bold">Awesomeness</div>
@@ -12,6 +36,12 @@ export default function TopBar() {
         {/* <Ai /> */}
         <ThemeChanger />
         <User />
+        <Button
+          className="ml-3"
+          onClick={() => {
+            queryClient.invalidateQueries({ queryKey: ["all-stars"] });
+          }}
+        ></Button>
       </div>
     </div>
   );
